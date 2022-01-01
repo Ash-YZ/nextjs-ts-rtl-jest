81db618b994b0ab7a89c2847e84ab8de6b7c1cb0
Ash <ash.yusufzai@inshur.com>
2022-01-01 21:23:35 +0000

    lint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,6 +11,9 @@ module.exports = {
     '@typescript-eslint/no-explicit-any': 'error',
     '@typescript-eslint/quotes': [2, 'single'],
     '@typescript-eslint/semi': [2, 'always'],
+    'react/jsx-props-no-spreading': [0],
+    'react/jsx-filename-extension': [0],
+    'react/react-in-jsx-scope': [0],
   },
   env: {
     jest: true,
