a09fc1c20300a73a602fd11ee1dbc2b4d4c1e884
Ash <ash.yusufzai@inshur.com>
2022-01-02 09:07:37 +0000

    lint config

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,8 +1,8 @@
 module.exports = {
   semi: true,
-  trailingComma: "es5",
+  trailingComma: 'es5',
   singleQuote: true,
   tabWidth: 2,
   useTabs: false,
-  printWidth: 100,
-}
+  printWidth: 80,
+};
