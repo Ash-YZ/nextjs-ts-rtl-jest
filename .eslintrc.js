6166f6ea7bdf39c9274e3add5e65b5d3b248493a
Ash <ash.yusufzai@inshur.com>
2022-01-02 09:18:22 +0000

    lint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,6 @@
 module.exports = {
   plugins: ['@typescript-eslint'],
-  extends: [
-    'next/core-web-vitals',
-    'plugin:@typescript-eslint/recommended',
-    'prettier',
-    'eslint-config-airbnb',
-  ],
+  extends: ['next/core-web-vitals', 'prettier', 'eslint-config-airbnb'],
   rules: {
     '@typescript-eslint/no-unused-vars': 'error',
     '@typescript-eslint/no-explicit-any': 'error',
@@ -14,6 +9,7 @@ module.exports = {
     'react/jsx-props-no-spreading': [0],
     'react/jsx-filename-extension': [0],
     'react/react-in-jsx-scope': [0],
+    'react/function-component-definition': [0],
   },
   env: {
     jest: true,
